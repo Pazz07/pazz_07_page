21a8251822a76520a348ec653362e199b622e344
Pablo Arvizu <92693448+Pazz07@users.noreply.github.com>
2022-10-03 00:14:55 -0500

    big page update

--- js/main.js
+++ js/main.js
@@ -15,3 +15,17 @@ function toggleClass(e){
         navBar.removeAttribute("style");
     }
 }
+
+let products = [];
+let total = 0;
+
+function add(product, price) {
+    console.log(product, price);
+    products.push(product);
+    total = total + price;
+    document.getElementById("checkout").innerHTML = `Pagar $${total}`
+}
+
+function pay() {
+    window.alert(products.join(", \n"));
+}
